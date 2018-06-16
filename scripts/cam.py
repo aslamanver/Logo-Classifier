aea2d2d8b1dad1ca6db5a609dfd75de46d5604ae
Aslam Anver <slam.anver9@gmail.com>
2018-06-16 05:37:01 +0000

    Training Code added

--- scripts/cam.py
+++ scripts/cam.py
@@ -11,6 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Train the logo classifier model with the images inside tf_files/logo_photos 
+
+# IMAGE_SIZE=224
+# ARCHITECTURE="mobilenet_0.50_${IMAGE_SIZE}"
+
+# python -m scripts.retrain \
+        #   --bottleneck_dir=tf_files/bottlenecks \
+        #   --how_many_training_steps=500 \
+        #   --model_dir=tf_files/models/ \
+        #   --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+        #   --output_graph=tf_files/retrained_graph.pb \
+        #   --output_labels=tf_files/retrained_labels.txt \
+        #   --architecture="${ARCHITECTURE}" \
+        #   --image_dir=tf_files/logo_photos
+
 # ==============================================================================
 
 from __future__ import absolute_import
